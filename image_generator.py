0e563b71398b84751bd296eeb9bb2dfc4a9b95fd
Qasim Saleem <qasimsaleeem317@gmail.com>
2025-08-28 11:01:47 +0500

    Fix PySide6 Qt dependencies and force offscreen rendering

--- image_generator.py
+++ image_generator.py
@@ -1,6 +1,8 @@
+import os
 from PySide6.QtGui import QFont, QFontDatabase, QImage, QPainter, QColor,QPainterPath
 from PySide6.QtCore import Qt, QRectF
 import tempfile
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 def create_image_qt_text_bg(
     text,
